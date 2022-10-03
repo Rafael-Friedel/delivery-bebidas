3969a169939aee0a9250152fdb76b3870bacac88
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-03 16:55:00 -0300

    chore: :hammer: Aprimoramento do layout do fluxo do cliente

--- front-end/src/components/DetailsProducts/THeadTableDetailsProducts.jsx
+++ front-end/src/components/DetailsProducts/THeadTableDetailsProducts.jsx
@@ -4,19 +4,19 @@ function THeadTableDetailsProducts() {
   return (
     <thead className="bg-gray-200 border-b-2 border-gray-200">
       <tr>
-        <th className="w-12 p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-12 p-2 text-sm font-semibold tracking-wide text-left">
           Item
         </th>
-        <th className="p-3 text-sm font-semibold tracking-wide text-center">
+        <th className="p-2 text-sm font-semibold tracking-wide text-center">
           Descrição
         </th>
-        <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-24 p-2 text-sm font-semibold tracking-wide text-left">
           Quantidade
         </th>
-        <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-28 p-2 text-sm font-semibold tracking-wide text-left">
           Valor Unitário
         </th>
-        <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+        <th className="w-24 p-2 text-sm font-semibold tracking-wide text-left">
           Sub-total
         </th>
       </tr>
--- front-end/src/components/DetailsProducts/TableDetailsProducts.jsx
+++ front-end/src/components/DetailsProducts/TableDetailsProducts.jsx
@@ -11,34 +11,33 @@ function TableDetailsProducts({ sale }) {
         <THeadTableDetailsProducts />
         <tbody className="bg-gray-50 border-b-2 border-gray-200">
           {sale.Products.map((p, index) => (
-            <tr key={ index }>
+            <tr key={ index } className="hover:text-white hover:bg-blue-400">
               <td
-                className="p-3 text-sm text-gray-700 text-center"
+                className="p-3 text-sm text-center "
                 data-testid={ `${testId}-item-number-${p.id}` }
               >
                 {p.id}
               </td>
               <td
-                className="p-3 text-sm text-gray-700 text-center"
+                className="p-3 text-sm text-center"
                 data-testid={ `${testId}-name-${p.id}"` }
               >
                 {p.name}
               </td>
               <td
-                className={ `p-3 text-sm text-gray-700 text-center
-          hover:text-white hover:bg-blue-600` }
+                className="p-3 text-sm text-center"
                 data-testid={ `${testId}-quantity-${p.id}` }
               >
                 {p.SalesProduct.quantity}
               </td>
               <td
-                className="p-3 text-sm text-gray-700 text-center"
+                className="p-3 text-sm text-center"
                 data-testid={ `${testId}-unit-price-${p.id}` }
               >
                 {`R$ ${Number(p.price).toFixed(2).replace('.', ',')}`}
               </td>
               <td
-                className="p-3 text-sm text-gray-700 text-center"
+                className="p-3 text-sm text-center"
                 data-testid={ `${testId}-sub-total-${p.id}` }
               >
                 {`R$ ${(+p.price * +p.SalesProduct.quantity)
@@ -50,9 +49,9 @@ function TableDetailsProducts({ sale }) {
         </tbody>
       </table>
       <p
-        className={ `absolute bottom-10 right-40 h-16 w-16 border border-red-500 
-      font-semibold rounded-md ml-4 px-4 py-2 outline-none w-40 h-12 text-red-600 
-      hover:text-white hover:bg-red-600` }
+        className={ `absolute bottom-10 right-20 h-16 w-16 border border-green-500 
+      font-semibold rounded-md ml-4 px-4 py-2 outline-none w-40 h-12 text-green-600 
+      hover:text-white hover:bg-green-600` }
         data-testid="customer_order_details__element-order-total-price"
       >
         {`Total: R$ ${sale.totalPrice.replace('.', ',')}`}
@@ -63,27 +62,6 @@ function TableDetailsProducts({ sale }) {
 
 TableDetailsProducts.propTypes = {
   sale: PropTypes.instanceOf(Object).isRequired,
-  // sale: PropTypes.shape({
-  //   totalPrice: PropTypes.string.isRequired,
-  //   Products: PropTypes.arrayOf(
-  //     PropTypes.shape({
-  //       id: PropTypes.number.isRequired,
-  //       name: PropTypes.string.isRequired,
-  //       status: PropTypes.string.isRequired,
-  //       saleDate: PropTypes.string.isRequired,
-  //       SalesProduct: PropTypes.shape({
-  //         quantity: PropTypes.number.isRequired,
-  //       }),
-  //     }),
-  //   ),
-  // }),
 };
-
-// TableDetailsProducts.defaultProps = {
-//   sale: PropTypes.shape({
-//     totalPrice: 0,
-//     products: [],
-//   }),
-// };
 
 export default TableDetailsProducts;
